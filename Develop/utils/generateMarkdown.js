72a25d7a0ef286c6f1f3e5de42bffd5ca9011416
Jorge Ramirez Anzaldo <jaanzaldo@hotmail.com>
2022-11-09 19:36:40 -0600

    Updating with functions to render license badge, link and section. Also including Questions section with Github user and email

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,9 +1,45 @@
+// TODO: Crear una función que devuelva una credencial de licencia según la licencia en la que se pasa
+// Si no hay licencia, devuelva una cadena vacía
+function renderLicenseBadge(license) {
+  let licensemod = license.replace(" ", "_");
+  licensemod = licensemod.replace("-", "--");
+  let licenseimg = "https://img.shields.io/badge/License-" + licensemod + "-blue.svg";
+  return licenseimg;
+}
 
+// TODO: Crear una función que devuelva el enlace de licencia
+// Si no hay licencia, devuelva una cadena vacía
+function renderLicenseLink(i) {
+  const licenseurl = ["unlicense.org/", "opensource.org/licenses/ISC", "opensource.org/licenses/MIT",
+  "opensource.org/licenses/MPL-2.0",  "opensource.org/licenses/Apache-2.0", "www.boost.org/LICENSE_1_0.txt",
+  "www.gnu.org/licenses/agpl-3.0", "www.gnu.org/licenses/gpl-3.0", "www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+  "www.gnu.org/licenses/lgpl-3.0", "www.gnu.org/licenses/fdl-1.3", "opensource.org/licenses/BSD-3-Clause", "opensource.org/licenses/BSD-2-Clause", 
+  "opensource.org/licenses/EPL-1.0", "opensource.org/licenses/IPL-1.0", "opendatacommons.org/licenses/by/", "opendatacommons.org/licenses/odbl/",
+  "opendatacommons.org/licenses/pddl/", "https://opensource.org/licenses/Artistic-2.0", "opensource.org/licenses/OFL-1.1",  
+  "www.wtfpl.net/about/", "opensource.org/licenses/Zlib"
+];
+
+  let url = licenseurl[i];
+  let completeurl = "https://" + url;
+  return completeurl;
+
+}
+
+// TODO: Crear una función que devuelva la sección de licencia de README
+// Si no hay licencia, devuelva una cadena vacía
+function renderLicenseSection(license, link) {
+  let licsection = `Esta aplicación se encuentra bajo la licencia ${license}. Para conocer más detalles sobre esta licencia entra al siguiente link:  
+  * [Link a Licencia](${link})`
+  return licsection;
+}
 
 // TODO: Crear una función para generar un markdown para README
 function generateMarkdown(data, i) {
-  
-  return `# ${data.Title}     
+  const badge = renderLicenseBadge(data.License);
+  const link = renderLicenseLink(i);
+  const lictext = renderLicenseSection(data.License, link);
+  const gittext = "https://github.com/" + data.GitHub;
+  return `# ${data.Title} ![License](${badge})     
 
 ## Table of Contents  
 * [Descripción](#descripción-de-la-aplicación)
@@ -30,8 +66,14 @@ ${data.Contributing}
 ${data.Test}  
 
 ## Licencia 
- 
+${lictext}  
 
+## Preguntas  
+* Si tienes dudas, puedes encontrar más información al acceder a mi perfil de Github mediante el siguiente Link: [${data.GitHub}](${gittext}). 
+* Para preguntas adicionales puedes contactarme por medio de la siguiente dirección de correo electrónico: ${data.Mail}.
+  * En el asunto del correo coloca el nombre de la aplicación sobre la que tienes preguntas.
+  * Por favor coloca preguntas puntuales e incluye cualquier ejemplo o imagen que ayude a comprender tus dudas para ofrecerte una mejor respuesta.
+  * Considera que es posible que tarde un par de días en contestar.
 `;
 }
 
